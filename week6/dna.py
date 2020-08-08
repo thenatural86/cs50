9ea0fceb0d7d633559a9fe96aeeb8f51a1ffe3b6
thenatural86 <philip.smalls86@gmail.com>
2020-08-07 22:43:53 -0400

    for loops

--- week6/dna.py
+++ week6/dna.py
@@ -4,5 +4,21 @@ import csv
 if len(argv) != 3:
     print("Get your weight up")
 else:
-    file = open(argv[1], "r")
-    print("Yeah kid!", argv[1])
+    csvFile = open(argv[1])
+    txtFile = open(argv[2])
+    # reads one row at a time
+    data = csv.reader(csvFile)
+    # reads whole file at once
+    seq = txtFile.read()
+
+    length = len(seq)
+    # print(length)
+
+    for i in range(length):
+        print(i)
+        for j in range(length):
+            # if seq[i:j] == seq[i + 4]:
+            print(seq[i:j])
+
+        # need to find most repeating substring
+# print(seq)
