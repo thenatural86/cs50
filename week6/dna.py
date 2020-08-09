7608d0ec342c0ebf0846bd76f1d8212effbceeea
thenatural86 <philip.smalls86@gmail.com>
2020-08-09 14:25:47 -0400

    print person

--- week6/dna.py
+++ week6/dna.py
@@ -17,7 +17,6 @@ def get_max(seq, sub):
         if seq[i:j] == sub:
             # create a temp counter var set to 0
             counter = 0
-            # while True:
             # while match is true
             while seq[i:j] == sub:
                 # update counter
@@ -35,29 +34,40 @@ def get_max(seq, sub):
     return maxi
 
 
+def print_match(data, max_data):
+    for i in data:
+        person = i[0]
+        values = [int(j) for j in i[1:]]
+        if values == max_data:
+            print(person)
+            return
+    print("Nope")
+
+
 def main():
     if len(argv) != 3:
         print("Get your weight up")
-    else:
-        csvFile = argv[1]
-        # if there is an error, with open will close file
-        with open(csvFile) as csv_file:
-            # file is read in to memeory via csv reader
-            # and return iterable object
-            data = csv.reader(csv_file)
-            # need to eleminate first row which is a header
-            row = next(data)[1:]
-            # for i in data:
-            #     print(i)
+        exit(1)
+
+    csvFile = argv[1]
+    # if there is an error, with open will close file
+    with open(csvFile) as csv_file:
+        # file is read in to memeory via csv reader
+        # and return iterable object
+        data = csv.reader(csv_file)
+        # need to eleminate first row which is a header
+        row = next(data)[1:]
+        # for i in data:
+        #     print(i)
 
         txtFile = argv[2]
         with open(txtFile) as txt_file:
             # read in entire file at once
             seq = txt_file.read()
-
+            # list comprehession
             max_data = [get_max(seq, sub) for sub in row]
-            print(seq)
-            get_max(seq, seq)
+
+        print_match(data, max_data)
 
 
 if __name__ == "__main__":
